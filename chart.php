61a12625d806af36891c2e3752e966fcb642203b
mkdodos <mkdodos@gmail.com>
2017-12-01 11:13:42 +0800

    Update chart.php

--- chart.php
+++ chart.php
@@ -4,3 +4,15 @@ Route::get('weight_chart_data', function () {
       ->get(['w_date','amt']);
     return $stats;
   });
+
+
+
+
+ $stats = DB::table('spending')
+        ->select('cate',DB::raw('SUM(expense) as price'),DB::raw('month(spend_date) as m'))
+        ->groupBy('cate')
+        ->groupBy(DB::raw('month(spend_date)'))
+        ->whereRaw('year(spend_date)=2017')
+        // ->where('cate','=','159')//水
+        ->Where('cate','=','122')//電
+        ->get();
